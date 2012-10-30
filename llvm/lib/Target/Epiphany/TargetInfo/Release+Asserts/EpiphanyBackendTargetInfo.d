03c7e877898d69eeb30d25c420aec7d3e77e5cf7
Abel Bernabeu <abel@abel-laptop.(none)>
2012-10-30 22:19:51 +0000

    Build fix.

---
+++ llvm/lib/Target/Epiphany/TargetInfo/Release+Asserts/EpiphanyBackendTargetInfo.d
@@ -0,0 +1,162 @@
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/lib/Target/Epiphany/TargetInfo/Release+Asserts/EpiphanyBackendTargetInfo.o \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/lib/Target/Epiphany/TargetInfo/Release+Asserts/EpiphanyBackendTargetInfo.d: \
+ EpiphanyBackendTargetInfo.cpp \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/lib/Target/Epiphany/TargetInfo/../EpiphanyTargetMachine.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetMachine.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/CodeGen.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetOptions.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/StringRef.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/type_traits.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/DataTypes.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetData.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Pass.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassSupport.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Pass.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassRegistry.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/InitializePasses.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Atomic.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Valgrind.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Compiler.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Config/llvm-config.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassAnalysisSupport.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/SmallVector.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Module.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Function.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalValue.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Constant.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/User.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/ErrorHandling.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Value.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Use.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/PointerIntPair.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Casting.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/CallingConv.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/BasicBlock.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Instruction.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ilist_node.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/DebugLoc.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/DenseMapInfo.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Instruction.def \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/SymbolTableListTraits.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ilist.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/Twine.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Argument.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Attributes.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/MathExtras.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/SwapByteOrder.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalVariable.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/OperandTraits.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalAlias.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Metadata.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ArrayRef.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/FoldingSet.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/OwningPtr.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/TargetRegistry.h \
+ /home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/Triple.h
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/lib/Target/Epiphany/TargetInfo/../EpiphanyTargetMachine.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetMachine.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/CodeGen.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetOptions.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/StringRef.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/type_traits.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/DataTypes.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Target/TargetData.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Pass.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassSupport.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Pass.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassRegistry.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/InitializePasses.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Atomic.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Valgrind.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Compiler.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Config/llvm-config.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Module.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Function.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalValue.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Constant.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/User.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Value.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Use.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/Casting.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/CallingConv.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/BasicBlock.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Instruction.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/DebugLoc.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Instruction.def:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/SymbolTableListTraits.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ilist.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/Twine.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Argument.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Attributes.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/MathExtras.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalVariable.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/OperandTraits.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/GlobalAlias.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Metadata.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/FoldingSet.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/OwningPtr.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/Support/TargetRegistry.h:
+
+/home/abel/Work/versioned/github/epiphany-opengl/llvm/include/llvm/ADT/Triple.h:
